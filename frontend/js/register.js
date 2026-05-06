b5a2e6968716afc9473dbb77d231e085a3f4758e
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-05 22:29:21 -0300

    Pegando objetos inputs

---
+++ frontend/js/register.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form");
+
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const emailErro = document.getElementById("emailErro");
+const senha = document.getElementById("senha");
+const senhaRepet = document.getElementById("senhaRepet");
+const senhaErro = document.getElementById("senhaErro");
+
+
+const botao = document.querySelector("button=[type='submit']");
+
