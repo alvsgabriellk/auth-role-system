9caa28b8dfb1c97a247257b2cbd03fca8a22922a
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-05 22:30:23 -0300

    Adicionando regras para senha

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -10,3 +10,11 @@ const senhaErro = document.getElementById("senhaErro");
 
 const botao = document.querySelector("button=[type='submit']");
 
+const regras = {
+    min: document.getElementById("min"),
+    maius: document.getElementById("maius"),
+    minus: document.getElementById("minus"),
+    numero: document.getElementById("numero"),
+    simbolo: document.getElementById("simbolo")
+};
+
