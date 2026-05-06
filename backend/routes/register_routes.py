cc4cef713e85756928efc15c05777728f9c65b16
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-06 10:52:19 -0300

    Entrada e retorno dos dados processados

--- backend/routes/register_routes.py
+++ backend/routes/register_routes.py
@@ -1,6 +1,15 @@
-from database import db
-from flask import Blueprint, request, redirect, url_for
-from database import Usuario
-from sqlalchemy.exc import IntegrityError
+from flask import Blueprint, request, jsonify
+from controllers import registrando_usuario
+
 
 register_bp = Blueprint("register", __name__)
+
+@register_bp.route("/sign-up", methods=["POST"])
+def novo_cadastro():
+    dados = request.get_json()
+
+    resultado, status = registrando_usuario(dados)
+
+    return jsonify(resultado), status
+
+   
