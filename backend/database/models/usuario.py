2bd56b6253c10d1fac11ec59a3d75dabc1b53203
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-06 08:49:05 -0300

    Tábela de usuário criada

---
+++ backend/database/models/usuario.py
@@ -0,0 +1,14 @@
+from database import db
+from datetime import datetime
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(35), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    senha = db.Column(db.String(256), nullable=False)
+    data_criado = db.Column(db.DateTime, default=datetime.utcnow)
+    data_ultima_atualizacao = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    role = db.Column(db.Enum("user", "admin"), default="user", nullable=False) 
+    ativo = db.Column(db.Boolean, default=True)
