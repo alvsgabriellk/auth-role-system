d7f165bd6ae7a7787451f7d61ce76cbc64b7a119
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-06 00:12:03 -0300

    (V) - Método para carregar banco local pra uso de desenvolvimento, e banco hospedado para uso de produção real

--- backend/app.py
+++ backend/app.py
@@ -7,4 +7,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:5500"]) # faz o back aceitar o front acessar a api
+CORS(app, origins=["http://127.0.0.1:5500"]) # faz o back aceitar o front e o front acessar a api
+
+app.secret_key = os.getenv("SECRET_KEY_API")
+
+ENV = os.getenv("ENV", "local") # se nao achar chave env, aceita default(local)
+
+if ENV == "production": # no servidor de deploy terá la ENV=production
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+        "connect_args": {"sslmode": "require"}
+    }
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
+
+db.init_app(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
