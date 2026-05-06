7bc5a9161424dc8434f443f899e2282d183468e9
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-06 09:17:25 -0300

    (V) - Criando reconhecimento de servidor pra validar url do front, e correção de arquivo env pra global

--- backend/app.py
+++ backend/app.py
@@ -7,11 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:5500"]) # faz o back aceitar o front e o front acessar a api
+
+ENV = os.getenv("ENV", "local") # lá vai ta production, e aqui local
+
+if ENV == "production":
+    WEB_URL = os.getenv("WEB_URL")
+    CORS(app, origins=[WEB_URL])
+else:
+    CORS(app, origins=["http://127.0.0.1:5500"])  # faz o back aceitar o front e o front acessar a api
 
 app.secret_key = os.getenv("SECRET_KEY_API")
-
-ENV = os.getenv("ENV", "local") # se nao achar chave env, aceita default(local)
 
 if ENV == "production": # no servidor de deploy terá la ENV=production
     DATABASE_URL = os.getenv("DATABASE_URL") # url do banco vai ta na env da plataforma 
