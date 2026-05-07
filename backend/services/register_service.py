7215424db867deef8c411af568d0b78a15611e91
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-06 22:51:36 -0300

    (V) - Modificando código pra retornar dados puro, sem jsonify

--- backend/services/register_service.py
+++ backend/services/register_service.py
@@ -2,6 +2,9 @@ from utils import gerar_senha_hash
 from sqlalchemy.exc import IntegrityError
 from database import db, Usuario
 
+# Responsabilidades:
+# lógica, banco, validação
+
 def usuario_criado(nome, email, senha):
     senha_hash = gerar_senha_hash(senha)
 
@@ -16,8 +19,9 @@ def usuario_criado(nome, email, senha):
         db.session.commit()
     except IntegrityError:
         db.session.rollback()
-        return {"erro": "Esse email já foi cadastrado"}, 409
+        return {"erro": "Esse email já foi cadastrado."}, 409
     
     return {"msg": "Você foi cadastrado com sucesso!"}, 201
     
+    # SEM JSONIFY, APENAS RETORNAR DADOS PUROS
 
