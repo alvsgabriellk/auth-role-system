82a8dad76ceedd65a608f0a5a09ce7ddf266567f
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-08 12:54:25 -0300

    (T) - Arquitetura de lógica do sistema pra receber requisições do site pra api e entregar pro usuário

---
+++ frontend/static/js/services/authService.js
@@ -0,0 +1,21 @@
+export async function cadastrarUsuario(usuario) {
+
+        // momento que sai do navegador e vai pra a api
+        const res = await fetch("http://127.0.0.1:5000/register/sign-up", {  // fetch = promisse
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(usuario)
+        });
+
+        const dados = await res.json(); // se retornar uma resposta(resolve)
+
+        return {
+            ok: res.ok,
+            dados
+        };
+}
+
+
+
