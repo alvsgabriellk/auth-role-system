1bbf7e17e85fd64cfcdef6640608df74a9cbf82c
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-08 12:52:54 -0300

    (T) - Arquitetura UI pra visual, função pra mostrar alertas do sistema, 3 estágios diferentes

---
+++ frontend/static/js/ui/alerts.js
@@ -0,0 +1,38 @@
+export function mostrarAlerta(tipo, mensagem) {
+
+    const alerta = document.getElementById("alerta");
+    const mensagemAlerta = document.getElementById("mensagemAlerta");
+    const iconeAlerta = document.getElementById("iconeAlerta");
+
+    alerta.className = 
+    "alert d-flex align-items-center";
+
+    if (tipo === "success") {
+        alerta.classList.add("alert-success");
+
+        iconeAlerta.setAttribute(
+            "href",
+            "#check-circle-fill"
+        );
+    } else if (tipo === "danger") {
+        alerta.classList.add("alert-danger");
+
+        iconeAlerta.setAttribute(
+            "href",
+            "#exclamation-triangle-fill"
+        );
+    } else if (tipo === "warning") {
+        alerta.classList.add("alert-warning");
+
+        iconeAlerta.setAttribute(
+            "href",
+            "#info-fill"
+        );
+    }
+
+    mensagemAlerta.textContent = mensagem;
+
+    setTimeout( () => {
+        alerta.classList.add("d-none");
+    }, 3000);
+}
