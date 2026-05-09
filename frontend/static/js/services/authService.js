88ff3532135abdb0efa3bcb6e0e8f49d6afd3f5a
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-09 01:11:09 -0300

    Modificando rota register -> auth

--- frontend/static/js/services/authService.js
+++ frontend/static/js/services/authService.js
@@ -1,7 +1,7 @@
 export async function cadastrarUsuario(usuario) {
 
         // momento que sai do navegador e vai pra a api
-        const res = await fetch("http://127.0.0.1:5000/register/sign-up", {  // fetch = promisse
+        const res = await fetch("http://127.0.0.1:5000/auth/sign-up", {  // fetch = promisse
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
