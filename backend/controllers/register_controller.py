609a6e11f4889be881ad0670a4e1e93d64282408
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-10 23:18:34 -0300

    Ajeitando nomenclatura adequada para controller

--- backend/controllers/register_controller.py
+++ backend/controllers/register_controller.py
@@ -1,11 +1,11 @@
 # CONTROLLER LIDA COM REQUISIÇÃO.
 # ENTRADA E SAIDA
 
-from services import usuario_criado
+from services import registrar_usuario
 
-def registrando_usuario(dados): # ENTRADA DOS DADOS DA REQUISIÇÃO
+def register_controller(dados): # ENTRADA DOS DADOS DA REQUISIÇÃO
     nome = dados["nome"]
     email = dados["email"]
     senha = dados["senha"]
 
-    return usuario_criado(nome, email, senha) # SAIDA DOS DADOS JÁ COM A LÓGICA
+    return registrar_usuario(nome, email, senha) # SAIDA DOS DADOS JÁ COM A LÓGICA
