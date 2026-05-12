c7b0dd00ec688f3b3a810278aa1213ddbb2b6245
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-11 23:55:44 -0300

    Melhorando a sintaxe e leitura do código hash

--- backend/utils/hash.py
+++ backend/utils/hash.py
@@ -3,5 +3,5 @@ from werkzeug.security import generate_password_hash, check_password_hash
 def gerar_senha_hash(senha):
     return generate_password_hash(senha)
 
-def verificar_senha_hash(senha, senha_registrada):
-    return check_password_hash(senha, senha_registrada)
+def verificar_senha_hash(senha_registrada, senha_digitada):
+    return check_password_hash(senha_registrada, senha_digitada)
