43a39bc8cfe1c58b7a2c1c4671f1739a4c165d11
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-18 15:23:18 -0300

    Criando e passando token de verificação de email

--- backend/services/register_service.py
+++ backend/services/register_service.py
@@ -2,8 +2,11 @@ from utils import gerar_senha_hash
 from sqlalchemy.exc import IntegrityError
 from database import db, Usuario
 import requests
+from flask import current_app
 from dotenv import load_dotenv
 import os
+from itsdangerous import URLSafeTimedSerializer # gerar e valida token
+from urllib.parse import quote # encoda token antes de colocar na url
 
 load_dotenv()
 
@@ -41,6 +44,10 @@ def enviar_email(email, link):
 # lógica, banco, validação
 
 def registrar_usuario(nome, email, senha):
+    #  cria um objeto pra usar minha chave e gerar/ler tokens
+    # só é criado quando a função é chamada
+    serializer = URLSafeTimedSerializer(current_app.config["SECRET_KEY_API"])
+
     senha_hash = gerar_senha_hash(senha)
 
     usuario = Usuario(
@@ -56,6 +63,12 @@ def registrar_usuario(nome, email, senha):
         db.session.rollback()
         return {"error": "Esse email já foi cadastrado."}, 409
     
+    token = serializer.dumps(email)
+    token_encoded = quote(token, safe="") # codifica os pontos e caract especias
+
+    link_confirmação = f"http://127.0.0.1:5000/auth/verificar-email/{token_encoded}"
+
+    enviar_email(email, link_confirmação)
     return {"msg": "Você foi cadastrado com sucesso!"}, 201
     
     # SEM JSONIFY, APENAS RETORNAR DADOS PUROS
