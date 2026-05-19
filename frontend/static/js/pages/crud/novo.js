a9388f34a91a77e844849f953745eb5ec0ad4baa
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-19 19:50:23 -0300

    Pegando os campos do formulário de novo produto

---
+++ frontend/static/js/pages/crud/novo.js
@@ -0,0 +1,15 @@
+const NovoForm = document.getElementById("novoProduto");
+
+const NomeNovoProduto = document.getElementById("NomeNovoProduto");
+const NovaDescricao = document.getElementById("NovaDescricao");
+const NovoPreco = document.getElementById("NovoPreco");
+const NovoEstoque = document.getElementById("NovoEstoque");
+const NovaCategoria = document.getElementById("NovaCategoria");
+
+const NovoProdutoErro = document.getElementById("NovoProdutoErro");
+const NovaDescricaoErro = document.getElementById("NovaDescricaoErro");
+const NovoPrecoErro = document.getElementById("NovoPrecoErro");
+const NovoEstoqueErro = document.getElementById("NovoEstoqueErro");
+const NovaCategoriaErro = document.getElementById("NovaCategoriaErro");
+
+const ButtonNovo = document.getElementById("ButtonNovo");
