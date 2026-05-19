a8875675f51cf5883955aedd14eb77335d1b18c5
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-19 19:42:16 -0300

    Corrigindo valores dos dados

--- backend/controllers/novo_produto_controller.py
+++ backend/controllers/novo_produto_controller.py
@@ -3,8 +3,8 @@ from services import adicionar_produto
 def novo_produto_controller(dados):
     nome = dados["nome"]
     descricao = dados["descricao"]
-    preco = dados["preco"]
-    estoque = dados["estoque"]
+    preco = float(dados["preco"])
+    estoque = int(dados["estoque"])
     categoria = dados["categoria"]
 
     return adicionar_produto(nome, descricao, preco, estoque, categoria)
