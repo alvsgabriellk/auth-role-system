d067f837ee87cba356e7f6a1783e685260533f1d
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-19 13:10:42 -0300

    Criando rota blueprint pra o sistema crud de produtos

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+crud_bp = Blueprint("crud", __name__, url_prefix="/crud")
 
 from .auth import ( 
     login_routes,
