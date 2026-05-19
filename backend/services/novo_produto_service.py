a6b0d52036df09dd8c1f2817a46b0c317e086480
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-19 19:46:27 -0300

    Salvando status com o produto com base no estoque

--- backend/services/novo_produto_service.py
+++ backend/services/novo_produto_service.py
@@ -1,15 +1,16 @@
-from database import Produto
-from database import db
-from sqlalchemy import select
+from database import db, Produto
+from utils import definir_status
 from sqlalchemy.exc import IntegrityError 
 
 def adicionar_produto(nome, descricao, preco, estoque, categoria):
+    status = definir_status(estoque)
     produto = Produto(
         nome=nome,
         descricao=descricao,
         preco=preco,
         estoque=estoque,
-        categoria=categoria
+        categoria=categoria,
+        status=status
     )
 
     try:
