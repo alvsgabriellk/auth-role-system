6e2fcaf5b2f295ee792811e8d6232aa57df4730d
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-19 13:13:51 -0300

    Tábela de produtos adicionada no database local/sqlalchemy

---
+++ backend/database/models/produto.py
@@ -0,0 +1,16 @@
+from database import db
+from datetime import datetime
+
+class Produto(db.Model):
+    __tablename__ = "produtos"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(30), nullable=False, unique=True)
+    descricao = db.Column(db.Text, nullable=False)
+    preco = db.Column(db.Numeric, nullable=False)
+    estoque = db.Column(db.Integer, nullable=False)
+    categoria = db.Column(db.String(20), nullable=False)
+    data_criado = db.Column(db.DateTime, default=datetime.utcnow)
+    data_ultima_atualizacao = db.Column(db.Datetime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    status = db.Column(db.Boolean, default=None)
+    
