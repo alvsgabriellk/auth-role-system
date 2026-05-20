0125329e6e4ce4b4c3b1c0d96b238ac94657e42d
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-19 22:09:53 -0300

    Pedindo autorização de token para requisitar a rota crud

--- backend/routes/crud/novo_routes.py
+++ backend/routes/crud/novo_routes.py
@@ -1,8 +1,10 @@
 from flask import request, jsonify
 from routes import crud_bp
 from controllers import novo_produto_controller
+from middlewares import token_requerido
 
 @crud_bp.route("/novo-produto", methods=["POST"])
+@token_requerido
 def novo_produto():
     dados = request.get_json()
 
