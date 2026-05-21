0104a7ad189e661d480dc53cf646203e65768528
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-20 23:06:34 -0300

    Passando funções para o init

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -2,3 +2,4 @@ from .register_service import registrar_usuario
 from .login_service import autenticar_usuario
 from .email_service import email_service
 from .novo_produto_service import adicionar_produto
+from .token_service import gerar_token, verificar_token
