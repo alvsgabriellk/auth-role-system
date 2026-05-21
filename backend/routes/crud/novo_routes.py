d8b3231176c0af376972fe7c74edc83b191ce685
alvsgabriellk <cauagabriell81@gmail.com>
2026-05-20 23:05:38 -0300

    Excluindo uso de funções na rota

--- backend/routes/crud/novo_routes.py
+++ backend/routes/crud/novo_routes.py
@@ -1,10 +1,8 @@
 from flask import request, jsonify
 from routes import crud_bp
 from controllers import novo_produto_controller
-from middlewares import token_requerido
 
 @crud_bp.route("/novo-produto", methods=["POST"])
-@token_requerido
 def novo_produto():
     dados = request.get_json()
 
